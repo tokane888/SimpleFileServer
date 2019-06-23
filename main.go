85ac26f4240bcdc0f174c61d3ec1dde07d814c6d
tokane888 <tokane888@gmail.com>
2019-06-23 20:21:12 +0900

    ファイファイルサーバのサンプル実装

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
